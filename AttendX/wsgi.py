b77cd7521223b82230f1cd3919bab9205f5c73e0
Shiekh Azmatulla <shiekazmatulla@gmail.com>
2026-04-18 11:29:29 +0530

    Structure Uploaded

---
+++ AttendX/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for AttendX project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AttendX.settings')
+
+application = get_wsgi_application()
